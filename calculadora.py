7191808335f6cccb2c66c1c77ecc9d805f4004da
LineeF12 <alinef.souza112@gmail.com>
2024-05-25 18:21:16 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+n1 = int(input("Digite um valor: "))
+n2 = int(input("Digite outro valor: "))
+
+soma = n1 + n2
+sub = n1 - n2
+mult = n1 * n2
+div = n1 / n2
+
+print("O resultado da adição de " ,n1, "+" ,n2, "é " ,soma)
+print("O resultado da subtração de " ,n1, "+" ,n2, "é " ,sub)
+print("O resultado da multiplicação de " ,n1, "+" ,n2, "é " ,mult)
+print("O resultado da divião de " ,n1, "+" ,n2, "é " ,div)
+
